638f876f9c7cee5fb4d6f6902e299ad44095b339
Tia Rose <shortyshort789@aol.com>
2021-02-21 09:02:32 -0800

    working on grid

--- src/components/items/items.js
+++ src/components/items/items.js
@@ -1,4 +1,7 @@
 import React, { useState, useContext, useEffect } from 'react';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
 import { ItemContext } from '../../context/itemsContext';
 
 function Items() {
@@ -18,16 +21,29 @@ function Items() {
           <h1>Nothing to display</h1>
         </div>
       ) : (
+        // <div>
+        // {context.items.map((item) => (
+        //   <div key={item.id}>
+        //     <h1>{item.name}</h1>
+        //     <h1>{item.description}</h1>
+        //     <h1>{item.price}</h1>
+        //     <img src={`${process.env.REACT_APP_S3_URL}/${item.name.split(' ').join('+')}`} alt={`${item.name}`} />
+        //   </div>
+        // ))}
+        // </div>
+
         <div>
-        {context.items.map((item) => (
-          <div key={item.id}>
-            <h1>{item.name}</h1>
-            <h1>{item.description}</h1>
-            <h1>{item.price}</h1>
-            <img src={`${process.env.REACT_APP_S3_URL}/${item.name.split(' ').join('+')}`} alt={`${item.name}`} />
-          </div>
-        ))}
-        </div>
+        <GridList cols={2.5}>
+          {context.items.map((item) => (
+            <GridListTile key={item.name}>
+              <img src={`${process.env.REACT_APP_S3_URL}/${item.name.split(' ').join('+')}`} alt={`${item.name}`} />
+              <GridListTileBar
+                title={item.name}
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
       )}
     </div>
   );
